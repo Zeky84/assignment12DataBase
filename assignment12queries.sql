24ab1d62cb27ad892af2b03eacd9c3e02a87af95
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-06-19 14:02:46 -0400

    last

--- assignment12queries.sql
+++ assignment12queries.sql
@@ -1,10 +1,10 @@
 --  Q4: Query to know how much money each customer spent
-SELECT  c.customer_name, SUM(p.pizza_cost * o.quantity) AS spent_by_customer FROM customers c
+SELECT  c.customer_name, SUM(p.pizza_cost * o.quantity) AS total_spent_by_customer FROM customers c
   JOIN orders o ON c.customer_id = o.customer_id
   JOIN pizzas p ON o.pizza_id = p.pizza_id
 GROUP BY
   c.customer_name
-ORDER BY spent_by_customer;
+ORDER BY total_spent_by_customer DESC;
 
 -- Q5: Query to know how much money each customer spent including the date and order number
 SELECT o.order_number, c.customer_name, SUM(p.pizza_cost * o.quantity) AS total_spent_by_customer, o.`date` 
@@ -19,5 +19,6 @@ ORDER BY  o.order_number;
 select * from orders o
 join customers c on c.customer_id = o.customer_id
 join pizzas p on p.pizza_id = o.pizza_id;
+
 
   
--- assignment12schema.sql
+++ assignment12schema.sql
@@ -1,20 +1,20 @@
 CREATE SCHEMA pizza_restaurant;
 
-CREATE TABLE pizzas (
+CREATE TABLE IF NOT EXISTS pizzas (
   pizza_id INT NOT NULL,
   pizza_name VARCHAR(45) NOT NULL,
   pizza_cost DECIMAL (5,2) NOT NULL,
   PRIMARY KEY (pizza_id)
 );
   
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
   customer_id INT NOT NULL,
   customer_name VARCHAR(100) NOT NULL,
   customer_phone VARCHAR(12) NOT NULL,
   PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
   order_id INT NOT NULL AUTO_INCREMENT,
   order_number INT NOT NULL,
   customer_id INT NOT NULL,
