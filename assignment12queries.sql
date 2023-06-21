1e6cc39631730d875c2601c305f8c9a925ba15bc
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-06-21 18:19:56 -0400

    Update assignment12queries.sql

--- assignment12queries.sql
+++ assignment12queries.sql
@@ -7,12 +7,12 @@ GROUP BY
 ORDER BY total_spent_by_customer DESC;
 
 -- Q5: Query to know how much money each customer spent including the date and order number
-SELECT o.order_number, c.customer_name, SUM(p.pizza_cost * o.quantity) AS total_spent_by_customer, DATE(o.`date`) as `date`
+SELECT c.customer_name, SUM(p.pizza_cost * o.quantity) AS total_spent_by_customer, DATE(o.`date`) as `date`
 FROM orders o
   JOIN customers c ON o.customer_id = c.customer_id
   JOIN pizzas p ON o.pizza_id = p.pizza_id
 GROUP BY
-  o.order_number, c.customer_id, c.customer_name, `date`
+  c.customer_id, c.customer_name, `date`
 ORDER BY  `date` DESC;
 
 -- Joining all tables for understanding
@@ -20,5 +20,7 @@ select * from orders o
 join customers c on c.customer_id = o.customer_id
 join pizzas p on p.pizza_id = o.pizza_id;
 
+
+select * from orders;  
 
   
